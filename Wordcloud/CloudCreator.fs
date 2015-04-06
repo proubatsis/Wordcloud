f771e1e41b40aecfd457d9a4208e5383622e921a
Panagiotis Roubatsis <proubatsis@gmail.com>
2015-04-06 11:27:53 -0400

    Modified alignment of words in the cloud.

--- Wordcloud/CloudCreator.fs
+++ Wordcloud/CloudCreator.fs
@@ -1,9 +1,10 @@
 ﻿module CloudCreator
     open System.Drawing
-    let rec findCoord (boxes:RectangleF list) size (cx, cy) (dx, dy) k =
+    let rec findCoord (boxes:RectangleF list) size (cx, cy) angle k =
+        let (dx, dy) = (cos angle, sin angle)
         let (x, y) = (dx * k + float cx, dy * k + float cy)
         let box = new RectangleF(new PointF(float32 x, float32 y), size)
-        if List.exists (fun r -> box.IntersectsWith r) boxes then findCoord boxes size (cx, cy) (dx, dy) (k + 1.0) else (x, y)
+        if List.exists (fun r -> box.IntersectsWith r) boxes then findCoord boxes size (cx, cy) (angle + System.Math.PI / 180.0) (k + 1.0) else (x, y)
 
     let rec drawCloud (g:Graphics) (boxes:RectangleF list) (cx, cy) angle colors = function
         | [] -> ()
@@ -16,10 +17,9 @@
                 g.DrawString(k, font, new SolidBrush(Seq.pick (fun c -> Some(c)) colors), box.X, box.Y)
                 drawCloud g [box] (cx, cy) angle (Seq.skip 1 colors) t
             | _ ->
-                let (dx, dy) = (cos angle, sin angle)
-                let (x, y) = findCoord boxes size (cx, cy) (dx, dy) 10.0
+                let (x, y) = findCoord boxes size (cx, cy) angle 10.0
                 let box = new RectangleF(float32 x, float32 y, size.Width, size.Height)
                 g.DrawString(k, font, new SolidBrush(Seq.pick (fun c -> Some(c)) colors), box.X, box.Y)
 
-                let k = 46.0 * (1.0 - v) + 2.0
+                let k = if v > 0.5 then 4.0 else 32.0
                 drawCloud g (List.append [box] boxes) (cx, cy) (angle + System.Math.PI / k) (Seq.skip 1 colors) t
